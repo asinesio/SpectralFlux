cf0e40b39f75457224ae02da3060041cb152eddf
Andy Sinesio <andy@precognitiveresearch.com>
2013-02-17 14:13:47 -0600

    Initial Commit

---
+++ SpectralFluxTests/SpectralFluxTests.h
@@ -0,0 +1,13 @@
+//
+//  SpectralFluxTests.h
+//  SpectralFluxTests
+//
+//  Created by Andy Sinesio on 2/17/13.
+//  Copyright (c) 2013 Precognitive Research, LLC. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SpectralFluxTests : SenTestCase
+
+@end
---
+++ SpectralFluxTests/SpectralFluxTests.m
@@ -0,0 +1,32 @@
+//
+//  SpectralFluxTests.m
+//  SpectralFluxTests
+//
+//  Created by Andy Sinesio on 2/17/13.
+//  Copyright (c) 2013 Precognitive Research, LLC. All rights reserved.
+//
+
+#import "SpectralFluxTests.h"
+
+@implementation SpectralFluxTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SpectralFluxTests");
+}
+
+@end
